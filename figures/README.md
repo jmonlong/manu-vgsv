3c59b236f9a358dd5ec05afe07a6e1410cb5032f
Jean Monlong <jmonlong@ucsc.edu>
2019-02-10 18:41:39 -0800

    hgsvc draft figures with tables as supp

--- figures/README.md
+++ figures/README.md
@@ -3,8 +3,8 @@ A place to put the data and scripts for figures.
 For now the figures are copied to the `content/images` folder. 
 
 ```sh
-## Rscript hgsvc-figures.R
+Rscript hgsvc-figures.R
 Rscript yeast-figures.R
 
-rsync *.svg ../content/images/
+rsync -v *.svg ../content/images/
 ```
--- figures/hgsvc-figures.R
+++ figures/hgsvc-figures.R
@@ -1,36 +1,68 @@
 library(ggplot2)
 
-
-## Read files and merge data adding a 'label' column
-readEval <- function(files, labels){
+## Function to read PR files and merge data adding a 'label' column
+readEval <- function(files, methods, regions=NULL){
+  if(is.null(regions)){
+    regions = rep('', length(methods))
+  }
   eval.df = lapply(1:length(files), function(ii){
     df = read.table(files[ii], as.is=TRUE, header=TRUE)
-    df$label = labels[ii]
+    df$method = methods[ii]
+    df$region = regions[ii]
     df
   })
   df = do.call(rbind, eval.df)
   ## order by quality to make sure the lines/paths are correctly drawn
   df = df[order(df$qual),]
+  df = subset(df, type!='INV' & TP.baseline>5)
+  df$type = factor(df$type, levels=c('Total', 'INS', 'DEL'))
   df
 }
 
-## For example for simulations and all regions
+## Simulated reads, all regions
 eval.df = readEval(files = c('sim-hgsvc-construct-prcurve.tsv',
-                             'sim-hgsvc-1kg-construct-prcurve.tsv',
-                             'sim-hgsvc-bayestyper-prcurve.tsv'),
-                   labels = c('HGSVC-construct',
-                              'HGSVC-1KG-constuct',
-                              'HGSVC-BayesTyper'))
+                             'sim-hgsvc-construct-clip-prcurve.tsv',
+                             'sim-hgsvc-bayestyper-prcurve.tsv',
+                             'sim-hgsvc-bayestyper-clip-prcurve.tsv'),
+                   methods = c('vg-construct', 'vg-construct',
+                              'BayesTyper', 'BayesTyper'),
+                   regions=c('all', 'non-repeat', 'all', 'non-repeat'))
 
-svg('hgsvc-sim.svg', 9, 7)
-ggplot(eval.df, aes(x=recall, y=precision, colour=label)) +
-  geom_path() +
-  ## geom_point(aes(size=qual)) +
+svg('hgsvc-sim.svg', 8, 4)
+ggplot(eval.df, aes(x=recall, y=precision, colour=method)) +
+  geom_path(aes(linetype=region), size=1, alpha=.8) + 
+  geom_point(size=.8) +
   theme_bw() +
-  facet_grid(.~label) +
-  xlim(0,1) + ylim(0,1) +
-  theme(legend.position='bottom')
+  facet_grid(.~type) +
+  theme(legend.position='bottom') +
+  scale_x_continuous(breaks=seq(0,1,.2), limits=0:1) + 
+  scale_y_continuous(breaks=seq(0,1,.2), limits=0:1) +
+  scale_linetype_manual(values=c(3,1)) + 
+  scale_colour_brewer(palette='Set1')
 dev.off()
 
 
-## Same for non-repeat regions and real reads.
+## Real reads, all regions
+eval.df = readEval(files = c('real-hgsvc-construct-prcurve.tsv',
+                             'real-hgsvc-construct-clip-prcurve.tsv',
+                             'real-hgsvc-bayestyper-prcurve.tsv',
+                             'real-hgsvc-bayestyper-clip-prcurve.tsv'),
+                   methods = c('vg-construct', 'vg-construct',
+                              'BayesTyper', 'BayesTyper'),
+                   regions=c('all', 'non-repeat', 'all', 'non-repeat'))
+
+svg('hgsvc-real.svg', 8, 4)
+ggplot(eval.df, aes(x=recall, y=precision, colour=method)) +
+  geom_path(aes(linetype=region), size=1, alpha=.8) + 
+  geom_point(size=.8) +
+  theme_bw() +
+  facet_grid(.~type) +
+  theme(legend.position='bottom') +
+  scale_x_continuous(breaks=seq(0,1,.2), limits=0:1) + 
+  scale_y_continuous(breaks=seq(0,1,.2), limits=0:1) +
+  scale_linetype_manual(values=c(3,1)) + 
+  scale_colour_brewer(palette='Set1')
+dev.off()
+
+
+## Same for non-repeat regions
