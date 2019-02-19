4d60145dc41316b61cad4d655ae255cefcf15098
jmonlong <jmonlong@ucsc.edu>
2019-02-19 07:31:25 -0800

    new vg version and handle inconsistent qualities

--- figures/simerror-figures.R
+++ figures/simerror-figures.R
@@ -3,12 +3,16 @@ library(dplyr)
 library(magrittr)
 
 eval.pr = read.table('simerror-prcurve.tsv', as.is=TRUE, header=TRUE)
-methods = c('toilvg', 'svtyper', 'delly', 'bayestyper')
-eval.pr$method = factor(eval.pr$method, levels=methods, labels=c('vg', 'svtyper', 'delly', 'bayestyper'))
 
-## Merge results across the three samples
-eval.pr = eval.pr %>% group_by(method, graph, depth, type, qual) %>%
-    select(-sample) %>% summarize_all(sum)
+methods = c('toilvg', 'svtyper', 'delly', 'bayestyper')
+eval.pr$method = factor(eval.pr$method, levels=methods, labels=c('vg', 'svtyper', 'Delly', 'BayesTyper'))
+
+## Merge results across the three samples picking the qual threshold with best F1
+## Not very elegant but otherwise I have make sure the same qual thresholds are used
+eval.pr = eval.pr %>% group_by(method, graph, depth, type, sample) %>%
+  arrange(desc(F1)) %>% do(head(., 1)) %>%
+  group_by(method, graph, depth, type) %>%
+  select(-sample, -qual) %>% summarize_all(sum)
 eval.pr$precision = eval.pr$TP.baseline / (eval.pr$TP.baseline + eval.pr$FP)
 eval.pr$recall = eval.pr$TP.baseline / (eval.pr$TP.baseline + eval.pr$FN)
 eval.pr$F1 = 2 * eval.pr$precision * eval.pr$recall / (eval.pr$precision + eval.pr$recall)
@@ -24,6 +28,16 @@ dp.lvls = paste(rep(unique(eval.df$depth), each=nlevels(eval.df$method)),
 eval.df$dp = factor(paste(eval.df$depth, eval.df$method), levels=dp.lvls)
 
 svg('simerror.svg', 8, 4)
+
+eval.df %>% ungroup %>%
+  mutate(graph=ifelse(graph=='truth', 'true SVs in VCF', 'errors in VCF'),
+         graph=factor(graph, levels=c('true SVs in VCF', 'errors in VCF'))) %>% 
+  ggplot(aes(x=factor(depth), y=F1, colour=method)) +
+  geom_line(aes(group=paste(method)), size=1, alpha=.8) +
+  facet_grid(graph~type, scales='free') + theme_bw() +
+  xlab('depth') + theme(legend.position='bottom') + 
+  scale_y_continuous(breaks=seq(0,1,.2), limits=0:1) +
+  scale_colour_brewer(palette='Set1')
 
 ## ggplot(eval.df, aes(x=factor(depth), y=F1, colour=method, shape=graph)) +
 ##   geom_point(aes(group=method), position=position_dodge(.5)) +
@@ -42,16 +56,6 @@ svg('simerror.svg', 8, 4)
 ##   scale_y_continuous(breaks=seq(0,1,.2), limits=0:1) +
 ##   scale_colour_brewer(palette='Set1')
 
-eval.df %>% ungroup %>%
-  mutate(graph=ifelse(graph=='truth', 'true SVs in VCF', 'errors in VCF'),
-         graph=factor(graph, levels=c('true SVs in VCF', 'errors in VCF'))) %>% 
-  ggplot(aes(x=factor(depth), y=F1, colour=method)) +
-  geom_line(aes(group=paste(method)), size=1, alpha=.8) +
-  facet_grid(graph~type, scales='free') + theme_bw() +
-  xlab('depth') + theme(legend.position='bottom') + 
-  scale_y_continuous(breaks=seq(0,1,.2), limits=0:1) +
-  scale_colour_brewer(palette='Set1')
-
 dev.off()
 
 ## Precision recall curve for 3x
@@ -68,9 +72,12 @@ eval.pr = read.table('simerror-max200bp-prcurve.tsv', as.is=TRUE, header=TRUE)
 methods = c('toilvg', 'svtyper', 'delly', 'bayestyper')
 eval.pr$method = factor(eval.pr$method, levels=methods, labels=c('vg', 'svtyper', 'delly', 'bayestyper'))
 
-## Merge results across the three samples
-eval.pr = eval.pr %>% group_by(method, graph, depth, type, qual) %>%
-    select(-sample) %>% summarize_all(sum)
+## Merge results across the three samples picking the qual threshold with best F1
+## Not very elegant but otherwise I have make sure the same qual thresholds are used
+eval.pr = eval.pr %>% group_by(method, graph, depth, type, sample) %>%
+  arrange(desc(F1)) %>% do(head(., 1)) %>%
+  group_by(method, graph, depth, type) %>%
+  select(-sample, -qual) %>% summarize_all(sum)
 eval.pr$precision = eval.pr$TP.baseline / (eval.pr$TP.baseline + eval.pr$FP)
 eval.pr$recall = eval.pr$TP.baseline / (eval.pr$TP.baseline + eval.pr$FN)
 eval.pr$F1 = 2 * eval.pr$precision * eval.pr$recall / (eval.pr$precision + eval.pr$recall)
