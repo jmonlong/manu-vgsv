5ce3750522b6146b928dca3cb1ae3f058c6b6d2d
jmonlong <jmonlong@ucsc.edu>
2019-03-19 11:39:45 -0700

    3 samples for hgsvc figs; giab fig but missing bayestyper

---
+++ figures/giab-figures.R
@@ -0,0 +1,83 @@
+library(ggplot2)
+library(dplyr)
+library(ggrepel)
+library(knitr)
+source('colors-functions.R')
+
+## Method names and renaming vector to fit color palette
+methods = c('vg','delly','svtyper')
+methconv = c(vg='vg', delly='Delly', svtyper='svtyper')
+
+## Simulated reads from HG00514
+samples = 'HG002'
+giab5.df = readEval4(methods, samples, prefix='data/giab/giab5')
+giab5.df$method = factor(methconv[giab5.df$method], levels=names(pal.tools))
+
+giab5.df = giab5.df %>% filter(type!='INV', type!='Total') %>% arrange(qual)
+label.df = giab5.df %>% group_by(region, method, type, eval) %>% arrange(desc(F1)) %>% do(head(.,1))
+
+pdf('pdf/giab5.pdf', 8, 4)
+
+giab5.df %>% filter(eval=='call') %>% 
+  ggplot(aes(x=recall, y=precision, colour=method)) +
+  geom_path(aes(linetype=region), size=1, alpha=.8) + 
+  geom_point(size=.8) +
+  ## geom_label_repel(aes(label=method), data=label.df) + 
+  geom_point(size=3, data=subset(label.df, eval=='call')) + 
+  theme_bw() +
+  facet_grid(.~type) +
+  theme(legend.position='bottom') +
+  ## scale_x_continuous(breaks=seq(0,1,.2), limits=0:1) + 
+  ## scale_y_continuous(breaks=seq(0,1,.1), limits=c(.6,1)) +
+  scale_linetype_manual(values=c(4,1)) + 
+  scale_colour_manual(values=pal.tools)
+
+dev.off()
+
+pdf('pdf/giab5-geno.pdf', 8, 4)
+
+giab5.df %>% filter(eval=='geno') %>% 
+  ggplot(aes(x=recall, y=precision, colour=method)) +
+  geom_path(aes(linetype=region), size=1, alpha=.8) + 
+  geom_point(size=.8) +
+  ## geom_label_repel(aes(label=method), data=label.df) + 
+  geom_point(size=3, data=subset(label.df, eval=='geno')) + 
+  theme_bw() +
+  facet_grid(.~type) +
+  theme(legend.position='bottom') +
+  ## scale_x_continuous(breaks=seq(0,1,.2), limits=0:1) + 
+  ## scale_y_continuous(breaks=seq(0,1,.1), limits=c(.6,1)) +
+  scale_linetype_manual(values=c(4,1)) + 
+  scale_colour_manual(values=pal.tools)
+
+dev.off()
+
+
+
+## Bar plots with best F1
+eval.f1 = label.df %>% ungroup %>%
+  mutate(F1=ifelse(is.infinite(F1), NA, F1),
+         eval=factor(eval, levels=c('call','geno'),
+                     labels=c('absence/presence', 'genotype')))
+  
+
+pdf('pdf/giab5-best-f1.pdf', 8, 4)
+
+eval.f1 %>% 
+  ggplot(aes(x=method, y=F1, fill=region, alpha=eval, group=region)) +
+  geom_bar(stat='identity', position=position_dodge()) +
+  facet_grid(type~.) +
+  scale_fill_brewer(name='genomic regions', palette='Set1') +
+  scale_alpha_manual(name='SV evaluation', values=c(.5,1)) + 
+  theme_bw() +
+  ylab('best F1') + 
+  theme(axis.text.x=element_text(angle=30, hjust=1),
+        axis.title.x=element_blank())
+
+dev.off()
+
+eval.f1 %>% filter(eval=='genotype', !is.na(F1)) %>%
+  select(method, region, type, precision, recall, F1) %>%
+  arrange(method, region) %>%
+  kable(digits=3) %>%
+  cat(file='tables/giab-geno-precision-recall-F1.md', sep='\n')
---
+++ figures/tables/giab-geno-precision-recall-F1.md
@@ -0,0 +1,12 @@
+|method  |region     |type | precision| recall|    F1|
+|:-------|:----------|:----|---------:|------:|-----:|
+|vg      |all        |INS  |     0.658|  0.646| 0.652|
+|vg      |all        |DEL  |     0.680|  0.643| 0.661|
+|vg      |non-repeat |INS  |     0.774|  0.735| 0.754|
+|vg      |non-repeat |DEL  |     0.768|  0.735| 0.751|
+|svtyper |all        |DEL  |     0.742|  0.342| 0.468|
+|svtyper |non-repeat |DEL  |     0.818|  0.496| 0.618|
+|Delly   |all        |INS  |     0.822|  0.177| 0.291|
+|Delly   |all        |DEL  |     0.722|  0.645| 0.681|
+|Delly   |non-repeat |INS  |     0.894|  0.268| 0.412|
+|Delly   |non-repeat |DEL  |     0.822|  0.768| 0.794|
